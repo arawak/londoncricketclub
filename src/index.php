2a32a663e92ed00a9d24c21d67951b464ce9d52b
Sean Mitchell <sean@mitchwood.com>
2022-01-24 21:03:08 -0500

    more updates

--- src/index.php
+++ src/index.php
@@ -1,16 +1,14 @@
 <?php include 'header.php' ?>
                 <h2 id="sec0">Over 150 years of cricket in London Ontario</h2>
 <p>London Cricket <a href="https://www.londonpubliclibrary.ca/blog/jiminy-cricket">dates back to at least 1856</a> when it was listed in the city's first directory. Over the years the club has had many homes, including Tecumseh Park, Fanshawe Park, the London Psychiatric Hospital grounds, Maitland Park, and most recently the North London Athletic Fields.</p>
-<p>We are members of the <a href="https://cricclubs.com/HDCL/">Hamilton &amp; District Cricket League</a> and in 2019 will be fielding teams the Premier and First divisions.</p>
+<p>We are members of the <a href="https://cricclubs.com/HDCL/">Hamilton &amp; District Cricket League</a> and in 2022 will be fielding teams in the Premier and Elite divisions.</p>
 <hr />
 <h3>Join Us</h3>
 <p>
 We play white ball 40 over matches in the Hamilton and District Cricket League, which operates under the ICC umbrella. Our members have represented HDCL, played for Ontario, and are eligible to play at the national level.
 </p>
-<p>
-Spaces are still available for regular and casual members of all skill levels. All equipment and transportation to away games is provided. <a href="https://forms.gle/6MLkvbSGMK1nNC2G6">Click here to sign up</a>, and get a free practice session before paying your membership fees.
-</p>
-<p>Registration is only open to local players (living in southwestern Ontario); we do not recruit from overseas. Join by completing <a href="https://goo.gl/forms/1lsYeD8E4IA2wQVL2">the registration form</a></p>
+<p>Registration is only open to local players (living in southwestern Ontario); <strong>we do not recruit from overseas</strong>. 
+Join by completing <a href="https://goo.gl/forms/1lsYeD8E4IA2wQVL2">the registration form</a></p>
 <div>
 </div>
 <div><a href="https://forms.gle/6MLkvbSGMK1nNC2G6" alt="click to register"><img src="img/register.png" class="img-responsive img-center" /></a></div>
@@ -52,27 +50,20 @@ Monday and Thursday nights are practices for the <u>league team players only</u>
 <p>
 We also play a small number of club matches and friendlies.
 </p>
-<p>
-Indoor training sessions start in January. These are held weekly at <a href="https://www.google.ca/maps/place/Centrefield+Sports+Inc,+25+Midpark+Crescent,+London,+ON+N6N+1A9">Centrefield Sports</a> near the 401. We will transition to outdoors as weather permits. $10 per session for paid members, $15 for non-members.
-</p>
                 <hr>
 <a name="membership"></a>
                 <h2 id="sec2">Membership</h2>
 <p>Membership is open to anyone who is interested, and lives in London, Canada. We are not in England. We do not hire professional players.</p>
 
-<h2>2018 Fees</h2>
+<h2>2022 Membership Fees</h2>
 <p>
-The base membership fee is $50. This entitles a member to vote, hold office, and register in the league teams. Paid members also are eligible to play in any club matches or friendly matches that are arranged.
-</p>
-<h3>HDCL League Fee</h3>
-<p>
-To play in the 40 over league teams, there is a fee to cover the costs of umpires, driving, food, and related expenses. The league fee is $220 and is over and above the base membership fee. This fee is discounted to $200 for students. New members will also require a uniform, which costs $42+HST.
+To be determined.
 </p>
 <hr />
 <p>
 Base membership fee is due upon joining for new members. Payment can be made by cheque, email transfer, or cash. Payment plans may be arranged with the treasurer</p>
 <p>
-Please note that there will be no proration of membership dues for past and current members. Any outstanding membership dues and other fees owing from previous seasons must be paid before 2018 membership dues.
+Please note that there will be no proration of membership dues for past and current members. Any outstanding membership dues and other fees owing from previous seasons must be paid before 2022 membership dues.
 </p>
                 <hr>
 <a name="contact"></a>
@@ -89,7 +80,7 @@ Please note that there will be no proration of membership dues for past and curr
                 </section>
                 <section id="content" class="span3">
                     <h3 class="comment-title">Email</h3>
-                    <p class="email">londoncricketclub@mitchwood.com</p>
+                    <p class="email">londoncricketclub1856@gmail.com</p>
 <p><em>We're not in England. We are not looking for overseas professional players. Anyone living in Southwestern Ontario, Canada is welcome to join.</em></p>
                 </section>
                 <hr>
